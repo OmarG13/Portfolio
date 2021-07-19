4bbb7eb7332b109803ac76f7a44b1d03b4c1a77e
OmarG13 <og13cs@gmail.com>
2021-07-19 17:07:42 +0800

    Gitup

--- student/scripts/portfolio/week1/gitup.sh
+++ student/scripts/portfolio/week1/gitup.sh
@@ -3,8 +3,8 @@
 # Simple script to commit and push into github
 # Github must already be set up
 # Git init must already have been run
-# $1="Folder to be added to github
-# $2="Commit description:
+# $1="Item to be added to github"
+# $2="Commit description"
 
 git add $1
 git commit -m "$2"
